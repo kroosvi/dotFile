816c8f10a6dc668c23c56e942f491db5f0794271
Kroos <kroosvi@gmail.com>
2023-01-22 09:45:32 -0500

    first commit of the day

--- nvim/lua/looks/UI-related.lua
+++ nvim/lua/looks/UI-related.lua
@@ -58,7 +58,7 @@ require('gitsigns').setup {
 require("everforest").setup({
   -- Controls the "hardness" of the background. Options are "soft", "medium" or "hard".
   -- Default is "medium".
-  background = "soft",
+  background = "hard",
   -- How much of the background should be transparent. Options are 0, 1 or 2.
   -- Default is 0.
   --
