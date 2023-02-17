feaf9cd828806b31a50c904748dccdc477bac05b
Kroos Xiang <kroosvi@gmail.com>
2023-02-16 22:00:10 -0500

    Change made

--- polybar/launch_night.sh
+++ polybar/launch_night.sh
@@ -5,4 +5,4 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 #excecute
-polybar main -c ~/.config/polybar/config.ini & 
+polybar main -c ~/.config/polybar/config_night.ini & 
