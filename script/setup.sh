15fcb7ea5378af4134fb763df55d3b6cab6f8581
Kroos Xiang <42981577+kroosvi@users.noreply.github.com>
2023-02-24 17:47:41 -0500

    Update setup.sh

--- script/setup.sh
+++ script/setup.sh
@@ -1,3 +1,7 @@
+
 sudo pacman -S neovim neofetch sxhkd bspwm feh zsh thunar bluez picom 
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 sudo pacman -S polybar rofi npm 
 sudo pacman -S adobe-source-han-sans-cn-fonts 
