4abbf0f726a7e1a5226c551c44639e7e290d9095
Kroos Xiang <42981577+kroosvi@users.noreply.github.com>
2023-02-24 20:35:38 -0500

    Update setup.sh

--- script/setup.sh
+++ script/setup.sh
@@ -7,5 +7,9 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/supercrabtree/k $ZSH_CUSTOM/plugins/k
 
-sudo pacman -S polybar rofi npm 
+sudo pacman -S polybar rofi npm rust firewalld
 sudo pacman -S adobe-source-han-sans-cn-fonts 
+
+yay -S spotify blugon nerd-fonts-complete-starship
+
+systemctl enable firewalld.service
